bd7d671b202f611d0aefc43f446eedcb4889e8e6
RuKi <163615835+RuKi08@users.noreply.github.com>
2025-04-05 10:16:00 +0900

    resize

--- Assets/code/heart_code.cs
+++ Assets/code/heart_code.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class heart_code : MonoBehaviour
 {
+    public float worldSize;
     private void OnEnable()
     {
-        transform.position = new Vector3(Random.Range(-50.0f, 50.0f), 2, Random.Range(-50.0f, 50.0f));
+        transform.position = new Vector3(Random.Range(-worldSize, worldSize), 2, Random.Range(-worldSize, worldSize));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y > 3 && transform.position.y < -1) transform.position = new Vector3(Random.Range(-50.0f, 50.0f), 2, Random.Range(-50.0f, 50.0f));
+        if(transform.position.y > 3 && transform.position.y < -1) transform.position = new Vector3(Random.Range(-worldSize, worldSize), 2, Random.Range(-worldSize, worldSize));
     }
 
     void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.tag == "terrain")
         {
-            gameObject.SetActive(false);
+            transform.position = new Vector3(Random.Range(-worldSize, worldSize), 2, Random.Range(-worldSize, worldSize));
         }
     }
 }
